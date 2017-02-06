578ae22c0bbfcc63a3fee7faeec2d1cb85c7d237
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-05 20:56:03 -0500

    it works but without file name

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import session from 'express-session';
+import multer from 'multer';
+var storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename(req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() +'.'+ file.mimetype.split('/')[1] )
+  }
+})
+
+var upload = multer({ storage: storage });
+
 mongoose.connect('mongodb://localhost/drive');
 const app = express();
 import driveApi from './controllers/drive';
@@ -21,12 +33,13 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    return res.sendFile(`${__dirname}/public/index.html`);
+  return res.sendFile(`${__dirname}/public/index.html`);
 });
 
 app.get('/api/v1/drive', driveApi.getAccess);
 app.get('/api/v1/drive/refresh', driveApi.refreshToken);
 app.get('/oauthCallback', driveApi.getTokens);
+app.post('/api/v1/drive/upload', upload.single('photo'), driveApi.upload);
 app.get('/api/v1/users', usersApi.getAll);
 app.post('/api/v1/users', usersApi.store);
 
--- controllers/drive.js
+++ controllers/drive.js
@@ -1,10 +1,13 @@
 import google from 'googleapis';
+import request from 'superagent';
+import axios from 'axios';
+import fs from 'fs';
 const OAuth2 = google.auth.OAuth2;
 const api = require('../client_id.json');
 const ClientId = api.web.client_id;
 const ClientSecret = api.web.client_secret;
 const RedirectionUrl = "http://localhost:4040/oauthCallback";
-var oauth2Client = new OAuth2(
+const oauth2Client = new OAuth2(
   ClientId,
   ClientSecret,
   RedirectionUrl
@@ -12,7 +15,10 @@ var oauth2Client = new OAuth2(
 
 const driveApi = {
 	getAccess(req, res) {
-		const scopes = ['https://www.googleapis.com/auth/drive.appfolder', 'https://www.googleapis.com/auth/drive.file'];
+		const scopes = [
+			'https://www.googleapis.com/auth/drive.appfolder', 
+			'https://www.googleapis.com/auth/drive.file'
+		];
 		
 		let url = oauth2Client.generateAuthUrl({
 			access_type: 'offline',
@@ -41,16 +47,39 @@ const driveApi = {
 		oauth2Client.refreshAccessToken(function(err, tokens) {
 			if (err) return console.error(err);
 			oauth2Client.setCredentials(tokens);
-			console.log(oauth2Client);
+
 			return res.json({success: true});
 		});
 	},
 
-	upload() {
-		var drive = google.drive({
-			version: 'v2',
-			auth: oauth2Client
-		});
+	upload(req, res) {
+		const {file} = req;
+		console.log(file);
+		// request
+		// .post('https://www.googleapis.com/upload/drive/v3/files?uploadType=media')
+		// .set('Content-Type', file.mimetype)
+		// .set('Content-Length', file.size)
+		// .set('Authorization', `Bearer ya29.GlzqA3ZgwEfbUjHcDAO9-4GM92DTIKAK7k86ZBq6T-xEdsamu6iJp3i_QbbI_vdxLxX26Z312mME3Ij-lloxrqoYzLO-sapH8ht_QYv8KBROMFZKV--gzl10o5u6mA`)
+		// .send({data: fs.createReadStream(`${__dirname.replace('controllers', '')}/${file.path}`)})
+		// .end((err, res) => console.log(res.body));
+		let token = 'ya29.GlzqA51tZSX7UHc5HxV7eiw1nGAxVYplsMcv8ZrbjVEpjmiAqGVfxZi644jtL5nO36g_tTvsAdkQ6CfYi4KVkq3FGVdRuQdHk6AI-oWR-y7enB-p83k7ZvJrREe-Sg';
+		axios
+		.post('https://www.googleapis.com/upload/drive/v3/files?uploadType=media', fs.createReadStream(`${__dirname.replace('controllers', '')}/${file.path}`), {	
+			headers: {
+				'Content-Type': file.mimetype,
+				'Content-Length': file.size,
+				'Authorization': `Bearer ${token}`
+			}}
+		)
+		.then(function (response) {
+			console.log(response.data);
+		})
+		.catch((err) => console.log(err));
+
+	},
+
+	download() {
+		`https://www.googleapis.com/drive/v3/files/${id}?alt=media`
 	}
 }
 
