8d8e2dc7955a8dc96bd2b8cd9ffd54cea908b5b9
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-06 14:34:34 -0500

    add update file name

--- controllers/drive.js
+++ controllers/drive.js
@@ -7,11 +7,7 @@ const api = require('../client_id.json');
 const ClientId = api.web.client_id;
 const ClientSecret = api.web.client_secret;
 const RedirectionUrl = "http://localhost:4040/oauthCallback";
-const oauth2Client = new OAuth2(
-  ClientId,
-  ClientSecret,
-  RedirectionUrl
-);
+const oauth2Client = new OAuth2( ClientId, ClientSecret, RedirectionUrl ); 
 
 const driveApi = {
 	getAccess(req, res) {
@@ -54,28 +50,34 @@ const driveApi = {
 
 	upload(req, res) {
 		const {file} = req;
-		console.log(file);
-		// request
-		// .post('https://www.googleapis.com/upload/drive/v3/files?uploadType=media')
-		// .set('Content-Type', file.mimetype)
-		// .set('Content-Length', file.size)
-		// .set('Authorization', `Bearer ya29.GlzqA3ZgwEfbUjHcDAO9-4GM92DTIKAK7k86ZBq6T-xEdsamu6iJp3i_QbbI_vdxLxX26Z312mME3Ij-lloxrqoYzLO-sapH8ht_QYv8KBROMFZKV--gzl10o5u6mA`)
-		// .send({data: fs.createReadStream(`${__dirname.replace('controllers', '')}/${file.path}`)})
-		// .end((err, res) => console.log(res.body));
-		let token = 'ya29.GlzqA51tZSX7UHc5HxV7eiw1nGAxVYplsMcv8ZrbjVEpjmiAqGVfxZi644jtL5nO36g_tTvsAdkQ6CfYi4KVkq3FGVdRuQdHk6AI-oWR-y7enB-p83k7ZvJrREe-Sg';
+		let token = 'ya29.GlzqAy9FyekVzXcU5NbZHgunRSr3EoIw8uUE82X414e3oKNewq0I1R626i9kVAC8cvXZgyofA8Q2CJSR3mTgnEil5DqS9cV5--dZPhK0AqEj_ByOYVg8wpUum2v_UA';
+		let data = fs.createReadStream(`${__dirname.replace('controllers', '')}/${file.path}`);
+
 		axios
-		.post('https://www.googleapis.com/upload/drive/v3/files?uploadType=media', fs.createReadStream(`${__dirname.replace('controllers', '')}/${file.path}`), {	
+		.post('https://www.googleapis.com/upload/drive/v3/files?uploadType=media', data, {	
 			headers: {
 				'Content-Type': file.mimetype,
 				'Content-Length': file.size,
 				'Authorization': `Bearer ${token}`
-			}}
-		)
-		.then(function (response) {
-			console.log(response.data);
+			}
+		})
+		.then(response => {
+			return driveApi.updateFileName(response.data.id, file.filename, token);
+		})
+		.then(response => {
+			return res.json({...response.data, url: `https://drive.google.com/file/d/${response.data.id}/view?usp=sharing`});
 		})
 		.catch((err) => console.log(err));
 
+	},
+
+	updateFileName(id, name, token) {
+		return axios
+		.patch(`https://www.googleapis.com/drive/v3/files/${id}`, {name}, {
+			headers: {
+				'Authorization': `Bearer ${token}`
+			}
+		});
 	},
 
 	download() {
